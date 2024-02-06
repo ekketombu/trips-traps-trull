72b93bec026f71f5c0bf3bdc3f4a9704366c2481
ekke <ekkemadistombu@gmail.com>
2024-02-06 17:15:15 +0200

    tertris üritan ühendada

---
+++ #tertris.py
@@ -0,0 +1,17 @@
+#tertris
+rida1=["1", "2", "3","|", "5","|", "7", "8", "9"]
+vahe ="---------"
+'''
+print(rida1[0-9]+"\n"+ vahe+ "\n"+rida1[0-9]+"\n"+ vahe+ "\n" +rida1[0-9])
+
+x_rida = input("Mis ritta soovid X-i panna? üles(ü), keskele(k), alla(a)")
+x_tulp = input("Mis tulpa soovid X-i panna? vasakule(v), keskele(k), paremale(p)")
+o_rida = input("Mis ritta soovid O-i panna? üles(ü), keskele(k), alla(a)")
+o_tulp = input("Mis tulpa soovid O-i panna? vasakule(v), keskele(k), paremale(p)")
+
+my_string = ''.join(map(str, rida1))
+
+print(my_string)
+'''
+#kui kolm järjestikust
+if 
